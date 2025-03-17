fac8d806e516bc028f272d877f03a0299cba00c2
nikhil <nikhil788p@gmail.com>
2025-03-17 19:06:56 +0530

    add load-task function

--- task-tracker-cli.py
+++ task-tracker-cli.py
@@ -6,7 +6,16 @@ file_name = "list.json"
 
 if not os.path.exists(file_name):
     with open(file_name, "w") as file:
-        json.dump({},file)
+        json.dump([],file, indent=4)
         print(f"{file_name} was created.")
-else:
-    print(f"{file_name} already exists")
+
+def load_tasks():
+    with open(file_name, "r") as file:
+        try:
+            data = json.load(file)
+            return data if isinstance(data, list) else []
+        except json.JSONDecodeError:
+            return []
+
+task = load_tasks()
+print(task)
