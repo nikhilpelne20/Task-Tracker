0318b85b81e47efce4ef94f4e8b72ac8e0e110c7
nikhil <nikhil788p@gmail.com>
2025-03-21 12:07:47 +0530

    improve code for cli commands

--- task-tracker-cli.py
+++ task-tracker-cli.py
@@ -1,5 +1,6 @@
 import os
 import json
+import argparse
 from datetime import datetime
 
 file_name = "list.json"
@@ -41,10 +42,14 @@ def add_task(task):
 
 def list_tasks():
     tasks = load_tasks()
-    task_list = []
+    if not tasks:
+        print("No Task found!")
+        return
+    print("All Tasks:")
     for task in tasks:
-        task_list.append(task['task'])
-    return task_list
+        print(f"[{task['id']}] {task['task']} - {task['progress']}")
+
+
 
 def update_task(task_id, updated_task):
     tasks = load_tasks()
@@ -52,8 +57,13 @@ def update_task(task_id, updated_task):
         if task['id'] == task_id:
             task['task'] = updated_task
             task['updatedAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    save_task(tasks)
-    print("Task updated successfully")
+            save_task(tasks)
+            print("Task updated successfully")
+            return
+    print(f"Task with ID {task_id} not Found.")
+
+
+
 
 def mark_in_progress(task_id):
     tasks = load_tasks()
@@ -61,8 +71,12 @@ def mark_in_progress(task_id):
         if task['id'] == task_id:
             task['progress'] = "in-progress"
             task['updatedAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    save_task(tasks)
-    print("Task updated successfully")
+            save_task(tasks)
+            print("Task marked in-progress successfully")
+            return
+    print(f"Task with ID {task_id} not Found.")
+
+
 
 def mark_done(task_id):
     tasks = load_tasks()
@@ -70,18 +84,39 @@ def mark_done(task_id):
         if task['id'] == task_id:
             task['progress'] = "done"
             task['updatedAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    save_task(tasks)
-    print("Task updated successfully")
+            save_task(tasks)
+            print("Task marked done successfully")
+            return 
+    print(f"Task with ID {task_id} not Found.")
+
+
+
+def mark_todo(task_id):
+    tasks = load_tasks()
+    for task in tasks:
+        if task['id'] == task_id:
+            task['progress'] = "todo"
+            task['updatedAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            save_task(tasks)
+            print("Task marked done successfully")
+            return 
+    print(f"Task with ID {task_id} not Found.")
+
 
 
 def list_task_status(status):
     tasks = load_tasks()
-    task_list = []
-    for task in tasks:
-        if task['progress'] == status:
-            task_list.append(task['task'])
-    
-    return task_list
+    filtered_task = [task for task in tasks if task["progress"] == status]
+    if not filtered_task:
+        print(f"no task with status: {status} found")
+        return
+    print(f"task with status: {status} :")
+    for task in filtered_task:
+        print(f"[{task['id']}] {task['task']}")
+
+
+def main():
+
 
 
 
